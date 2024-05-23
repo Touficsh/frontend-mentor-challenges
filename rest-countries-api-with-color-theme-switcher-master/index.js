b1a4b172ab119a326f67bf14a7eb0045cb07d5ee
Touficsh <toufic.charafeddine@gmail.com>
2024-05-23 23:46:18 +0300

    Integrate the theme switcher, Navbar, Search bar and Filter

---
+++ rest-countries-api-with-color-theme-switcher-master/index.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const themeToggle = document.getElementById('themeToggle');
+    const body = document.body;
+  
+    themeToggle.addEventListener('click', function() {
+      body.classList.toggle('dark');
+    });
+  });
+  
